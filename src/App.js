283b35d23b9ffe2c19551073bb0f6ad5fbb7d07c
faisal86 <faisalkarim203@gmail.com>
2023-12-28 15:07:50 +0300

    signup/sigin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Signin from './components/user/Signin';
+import Signup from './components/user/Signup';
+import {Routes, Route, Link} from "react-router-dom"
+import  Axios  from 'axios';
+import {jwtDecode} from "jwt-decode"
 
 function App() {
   return (
     <div className="App">
       <h1>Shop Swift App !</h1>
+      <div>
+        <Signin></Signin>
+      </div>
     </div>
   );
 }
