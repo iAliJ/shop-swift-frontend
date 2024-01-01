dba410a264b95452bf349cdb5d44a6fc821cb081
alij <alimjahrami@gmail.com>
2024-01-01 18:25:21 +0300

    feature - edit image shop by uploading it to backend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 import { useState, useEffect } from 'react';
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import  Axios  from 'axios';
