dd9d1e0fe63e030876f2d7a3de53bed83d704cfe
kmajeed85 <cokhalid@outlook.com>
2024-01-02 17:08:22 +0300

    seller register

--- src/components/user/Signup.jsx
+++ src/components/user/Signup.jsx
@@ -1,16 +1,33 @@
+import { func } from 'prop-types';
 import React, { useState } from 'react'
+
+
 export default function Signup(props) {
   // TODO... handle changes from user role selected item
+  const [options, setOptions] = useState([]);
+  const [role, setRole] = useState('');
   const [newUser, setNewUser] = useState({})
+  console.log(newUser);
+  
   const handleChange = (e) => {
     const user = {...newUser};
     user[e.target.name] = e.target.value;
     setNewUser(user);
   }
+
+
   const registerHandler = (e) => {
     e.preventDefault();
     props.register(newUser);
   }
+
+  const handleSelectChange = (e) =>{ 
+    setRole(e.target.value)
+    const user = {...newUser};
+    user.role = role;
+    setNewUser(user);
+  }
+
   return (
     <div className='form-signup col-4 mx-auto mt-3 text-center'>
       <h1>Sign Up</h1>
@@ -37,12 +54,14 @@ export default function Signup(props) {
         </div>
         <div>
           <label>User Role</label>
-          <select name="userRole"  onChange={handleChange} className='form-control'>
+          <select name="userRole"  onChange={handleSelectChange} className='form-control'>
             <option value="">Select Role</option>
-            <option value="Buyer">Buyer</option>
-            <option value="Seller">Seller</option>
+            <option value="buyer">buyer</option>
+            <option value="seller">seller</option>
           </select>
-        </div>
+
+            </div>
+       
         <div>
             {/* I dont think this is needed */}
           <label>Company Name</label>
@@ -55,7 +74,3 @@ export default function Signup(props) {
     </div>
   )
 }
-
-
-
-
