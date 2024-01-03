6b11926905c7282609db297cf2c6a643d392b452
alij <alimjahrami@gmail.com>
2024-01-03 23:29:05 +0300

    changes

--- src/Pages/dashboard/OrdersPage.jsx
+++ src/Pages/dashboard/OrdersPage.jsx
@@ -2,11 +2,18 @@ import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import CustomerOrder from '../../components/order/CustomerOrder';
 import SellerOrder from '../../components/order/SellerOrder';
-export default function Orders(props) {
+export default function OrdersPage(props) {
+
+  console.log('OrdersPage props:', props);
   return (
     <>
     {
-      (props.userData.role === 'buyer') ? <CustomerOrder userData={props.userData} headers={props.headers}/> : <SellerOrder userData={props.userData} headers={props.headers}/>
+      (props.userData.role === 'buyer') ? 
+      <CustomerOrder userData={props.userData} headers={props.headers}/> 
+      : 
+      <>
+      <SellerOrder userData={props.userData} headers={props.headers}/>
+      </>
     }
     </>
   )
--- src/components/order/SellerOrder.jsx
+++ src/components/order/SellerOrder.jsx
@@ -1,38 +1,89 @@
+// import React, { useState, useEffect } from 'react';
+// import Axios from 'axios';
+// export default function SellerOrder(props) {
+//   // Get orders by the user
+//   const [store, setStore] = useState({});
+//   const [orders, setOrders] = useState([]);
+
+//   useEffect(() => {
+//     getStore();
+//     getOrders();
+//   }, [props])
+
+//   function getOrders() {
+//     // get the store id owned by this seller
+//     Axios.get(`/orderitem/store?id=${store._id}`, props.headers)
+//     .then(res => {
+//         setOrders(res.data.orders);
+//     })
+//   }
+
+//   function getStore() {
+//     Axios.get(`/user/store?user=${props.userData._id}`, props.headers)
+//     .then(res => {
+//         setStore(res.data.store);
+//     })
+//   }
+
+//   const allOrders = orders.map((order, index) => (
+//     <tr key={index}>
+//         <td>{order._id}</td>
+//         <td>{order.product._id}</td>
+//         <td>{order.product.name}</td>
+//         <td>{order.quantity}</td>
+//         <td>{order.status}</td>
+//     </tr>
+// ));
+
+//   return (
+//     <div>
+//       <h1>Orders inside dashboard</h1>
+//       <table class="table">
+//         <thead>
+//           <th scope='col'>Order ID</th>
+//           <th scope='col'>Product ID</th>
+//           <th scope='col'>Product Name</th>
+//           <th scope='col'>Quantity</th>
+//           <th scope='col'>Status</th>
+//         </thead>
+//         <tbody>
+//           {allOrders}
+//         </tbody>
+//       </table>
+//     </div>
+//   )
+// }
+
+
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 export default function SellerOrder(props) {
 
   // Get orders by the user
-  const [user, setUser] = useState(props.userData);
-  const [store, setStore] = useState(loadStore());
-  const [orders, setOrders] = useState(loadOrders());
-
+  const [orders, setOrders] = useState([]);
+  const [store, setStore] = useState({});  
   useEffect(() => {
-    setUser(props.userData);
+    getOrders();
   }, [props])
 
-  function loadOrders() {
-    // get the store id owned by this seller
-    Axios.get(`/orderitem/store?id=${store._id}`, props.headers)
+  function getOrders() {
+    Axios.get(`/order/user?id=${props.userData._id}`, props.headers)
     .then(res => {
-        return res.data.orderItems;
+      setOrders(res.data.orders);
     })
   }
 
-  function loadStore() {
-    console.log(props.userData._id);
+  function getStore() {
     Axios.get(`/user/store?user=${props.userData._id}`, props.headers)
     .then(res => {
-        return res.data.store;
+        setStore(res.data.store);
     })
   }
 
   const allOrders = orders.map((order, index) => (
     <tr key={index}>
         <td>{order._id}</td>
-        <td>{order.product._id}</td>
-        <td>{order.product.name}</td>
-        <td>{order.quantity}</td>
+        <td>BHD {order.price}</td>
         <td>{order.status}</td>
     </tr>
 ));
@@ -40,12 +91,11 @@ export default function SellerOrder(props) {
   return (
     <div>
       <h1>Orders inside dashboard</h1>
+        <p>{store._id}</p>
       <table class="table">
         <thead>
           <th scope='col'>Order ID</th>
-          <th scope='col'>Product ID</th>
-          <th scope='col'>Product Name</th>
-          <th scope='col'>Quantity</th>
+          <th scope='col'>Total Price</th>
           <th scope='col'>Status</th>
         </thead>
         <tbody>
